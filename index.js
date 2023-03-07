84ad58c8182097bf1425590751cfacad2189017c
Mohammad Yaasir Cheekoory <77941119+yaasir007@users.noreply.github.com>
2023-03-07 23:28:19 +0400

    selected socials individually

---
+++ index.js
@@ -0,0 +1,6 @@
+const linkedin = document.querySelector("#linkedin");
+const github = document.querySelector("#github");
+const fb = document.querySelector("#fb");
+const insta = document.querySelector("#insta");
+const twitter = document.querySelector("#twitter");
+const resume = document.querySelector("#resume");
